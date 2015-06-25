3e4e35fdf69ea42b575e11047e7375b808bc00b9
belous <belous@amk-web.com>
2015-06-25 20:34:58 +0300

    добавил sonataAdminBundle

---
+++ src/ShopBundle/Entity/Product.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ShopBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Product
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Product
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
